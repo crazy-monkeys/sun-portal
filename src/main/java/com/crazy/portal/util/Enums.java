ca02f7fe9ac04fc5d839276d6062da1a2966d5e4
biao.chen <biao.chen@baozun.com>
2019-11-29 00:21:21 +0800

    国际化资源文件

--- src/main/java/com/crazy/portal/util/Enums.java
+++ src/main/java/com/crazy/portal/util/Enums.java
@@ -77,74 +77,74 @@ public class Enums {
     }
 
     //prod
-    public enum COUNTRY_MAIL{
-        AU("AU","service@sungrowpower.com.au","AUD"),
-        CL("CL","latam.service@sa.sungrowpower.com",""),
-        DE("DE","service.germany@sungrow-emea.com",""),
-        FR("FR","service.france@sungrow.co",""),
-        GR("GR ","service.greece@sungrow.co",""),
-        ES("ES","service.spain@sungrow-emea.com",""),
-        IN("IN","service@in.sungrowpower.com",""),
-        IT("IT","service.italy@sungrow-emea.com",""),
-        JP("JP","service@jp.sungrowpower.com",""),
-        MX("MX","techsupport@sungrow-na.com",""),
-        AE("AE","info@sungrow.cn",""),
-        TH("TH","service@th.sungrowpower.com",""),
-        BR("BR","latam.service@sa.sungrowpower.com",""),
-        KR("Korea","service@kr.sungrowpower.com",""),
-        PH("PH","service@ph.sungrowpower.com",""),
-        CN("CN","info@sungrow.cn",""),
-        GB("GB","service.uk@sungrow-emea.com",""),
-        RO("RO","service.romania@sungrow.co",""),
-        MY("MY","service@my.sungrowpower.com",""),
-        ZA("ZA","info@sungrow.cn",""),
-        TR("TR","service.turkey@sungrow.co",""),
-        AC("US","techsupport@sungrow-na.com","USD"),
-        VN("VN","service@vn.sungrowpower.com","");
-
-        private String id;
-        private String key;
-        private String currency;
-
-        public static String getKey(String id){
-            for(COUNTRY_MAIL e : COUNTRY_MAIL.values()){
-                if(id.equals(e.getId())){
-                    return e.getKey();
-                }
-            }
-            return null;
-        }
-
-        public static String getCurrency(String id){
-            for(COUNTRY_MAIL e : COUNTRY_MAIL.values()){
-                if(id.equals(e.getId())){
-                    return e.getCurrency();
-                }
-            }
-            return null;
-        }
-
-        COUNTRY_MAIL(String id, String key, String currency){
-            this.id = id;
-            this.key = key;
-            this.currency = currency;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getCurrency() {
-            return currency;
-        }
-    }
+//    public enum COUNTRY_MAIL{
+//        AU("AU","service@sungrowpower.com.au","AUD"),
+//        CL("CL","latam.service@sa.sungrowpower.com",""),
+//        DE("DE","service.germany@sungrow-emea.com",""),
+//        FR("FR","service.france@sungrow.co",""),
+//        GR("GR ","service.greece@sungrow.co",""),
+//        ES("ES","service.spain@sungrow-emea.com",""),
+//        IN("IN","service@in.sungrowpower.com",""),
+//        IT("IT","service.italy@sungrow-emea.com",""),
+//        JP("JP","service@jp.sungrowpower.com",""),
+//        MX("MX","techsupport@sungrow-na.com",""),
+//        AE("AE","info@sungrow.cn",""),
+//        TH("TH","service@th.sungrowpower.com",""),
+//        BR("BR","latam.service@sa.sungrowpower.com",""),
+//        KR("Korea","service@kr.sungrowpower.com",""),
+//        PH("PH","service@ph.sungrowpower.com",""),
+//        CN("CN","info@sungrow.cn",""),
+//        GB("GB","service.uk@sungrow-emea.com",""),
+//        RO("RO","service.romania@sungrow.co",""),
+//        MY("MY","service@my.sungrowpower.com",""),
+//        ZA("ZA","info@sungrow.cn",""),
+//        TR("TR","service.turkey@sungrow.co",""),
+//        AC("US","techsupport@sungrow-na.com","USD"),
+//        VN("VN","service@vn.sungrowpower.com","");
+//
+//        private String id;
+//        private String key;
+//        private String currency;
+//
+//        public static String getKey(String id){
+//            for(COUNTRY_MAIL e : COUNTRY_MAIL.values()){
+//                if(id.equals(e.getId())){
+//                    return e.getKey();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        public static String getCurrency(String id){
+//            for(COUNTRY_MAIL e : COUNTRY_MAIL.values()){
+//                if(id.equals(e.getId())){
+//                    return e.getCurrency();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        COUNTRY_MAIL(String id, String key, String currency){
+//            this.id = id;
+//            this.key = key;
+//            this.currency = currency;
+//        }
+//
+//        public String getId() {
+//            return id;
+//        }
+//
+//        public String getKey() {
+//            return key;
+//        }
+//
+//        public String getCurrency() {
+//            return currency;
+//        }
+//    }
 
     //Test
-   /* public enum COUNTRY_MAIL{
+    public enum COUNTRY_MAIL{
         AU("AU","ranson.xie@acloudear.com","AUD"),
         CL("CL","ranson.xie@acloudear.com",""),
         DE("DE","ranson.xie@acloudear.com",""),
@@ -208,10 +208,9 @@ public class Enums {
         public String getCurrency() {
             return currency;
         }
-    }*/
-
+    }
     //Prod
-    public enum API_PARAMS{
+    /*public enum API_PARAMS{
         Installer("C80E939416524035B7BCABB3EBAE2832","installInstaller"), //安装人
         Installation_Date("6B79D51956514828891DA1C2FDCD6348","installDate"),  //安装时间
         CEC("9CC1B99FB8254E59BD5D6E3E979BB4AE","installCec"),  //cec
@@ -274,73 +273,73 @@ public class Enums {
         public String getKey() {
             return key;
         }
-    }
+    }*/
 
     //Test
-//    public enum API_PARAMS{
-//        Installer("DC79C374404640FC80DCAC220F4B0015","installInstaller"), //安装人
-//        Installation_Date("E63BC043F4E9493B8FC8FE4421D7D292","installDate"),  //安装时间
-//        CEC("F92DF7E17DE246BE8EDF9DA23856E8C9","installCec"),  //cec
-//        ABN("5268B5F3CF964C7EA9AEC91E869A89E4","abn"),   //abn
-//        Customer_Contact("27D073A5FCFA4F6398D74B230595B078","customerContact"),  //维保公司名
-//        Contact_Email("4AFC9CD0CD8C4AA7B9921B6EC4645B95","contactEmial"),   //邮件
-//        CC_Email("2AEAA42EFDC34CC18C2E8BA2EC498550","ccEmail"),   //邮件
-//        Contact_Number("EB7AF9DC6E8D49FC98DC67ACE96BFFB6","contactNumber"), //number
-//        Customer_Address("9AD25F4EAEE54EAC86D6D7DD2E2FDA2E","customerAddress"), //安装地址
-//        Business_Name("A20288A9EAD24CC28564B4EEA7B7968F","businessName"), //终端客户
-//        Reference("0BB75D000E45404FAA3E2AB8189B1301","reference"),
-//        Electrical_Compliance_Certificate("1029856393994F4695065FB1A76D00B9","electricalComplianceCertificate"),
-//        Invoice_Uploaded("88D2D00FB3544918942470A7A3F6281C","invliceUpload"),
-//        Fault_Type("CF85B3DC233E4B70852F409812713A37","faultType"),
-//        Fault_Description("D31C9BE662904521A5A7577311D282FF","faultDescription"),
-//        Status_Message_LCD("99763AADA25D422089AAAB338C49E848","lcdMessage"),
-//        Inverter_Exposed_Weather("776769F766484874ADD9354E01AF0F16","isWeather"),
-//        Intervter_Connect_Battery("B029CDE660DA4053BACC2322E90B29CD","battery"),
-//        Post_Code("49D5913D6552439FABBCE76A26B57014","postCode"),
-//        Country("48B4E9768FE14BC9B60D231B80893A36","contryCode"),
-//        AMOUNT_EXCL_GST("DF5B2DC1E98F4509B11D866FFB2371C2","amountGST"),
-//        GST("0A652A2381B94F5A8BA42C97970201EB","gst"),
-//        TOTAL_AMOUNT("0CF10A5B6EA94732A8253B0C801E9249","totalAmount"),
-//        PURCHASE_ORDER("248246FD02874D34B0CA85E8996C0DA4","purchaseOrder"),
-//        SHIPPING_ADDRESS("042D3DDDD048464EA6B39E31527B926F","shippingAddress"),
-//        CURRENCY_CODE("4A3E10C5776048BC984D45DA47026A37","currency"),
-//        INVERTER_INSTALLATION_LOCATION("E6DB2E39349C4FEBB681500A674DF80D","location"),
-//        BATTERY_MODEL("C65359DD9C8B49BFA12462263498CFF7","batteryModel"),
-//
-//        Product_id("8E6F00CE608B410AADCFBC378B83F4C2",""),
-//        Delivery_date("5D66B7A7012843A49E9B2FF11F49C972",""),
-//        Dispatched_Date("8073A0B54A9F492BABC49D02EDB48BA6",""),
-//        accessory("528203E7CF6B4695BF9CD6CA6B9D0E14","accessory"),
-//
-//        Parts_Warranty_W5YP("A1B8254717004CF8A40E405B216352B5","W5YP"),
-//        Parts_Warranty_W5YS("31FF6AE5353D4D498A7C68EF20454456","W5YS"),
-//        ;
-//
-//        private String id;
-//        private String key;
-//
-//        public static String getId(String key){
-//            for(API_PARAMS e : API_PARAMS.values()){
-//                if(key.equals(e.getKey())){
-//                    return e.getId();
-//                }
-//            }
-//            return null;
-//        }
-//
-//        API_PARAMS(String id, String key){
-//            this.id = id;
-//            this.key = key;
-//        }
-//
-//        public String getId() {
-//            return id;
-//        }
-//
-//        public String getKey() {
-//            return key;
-//        }
-//    }
+    public enum API_PARAMS{
+        Installer("DC79C374404640FC80DCAC220F4B0015","installInstaller"), //安装人
+        Installation_Date("E63BC043F4E9493B8FC8FE4421D7D292","installDate"),  //安装时间
+        CEC("F92DF7E17DE246BE8EDF9DA23856E8C9","installCec"),  //cec
+        ABN("5268B5F3CF964C7EA9AEC91E869A89E4","abn"),   //abn
+        Customer_Contact("27D073A5FCFA4F6398D74B230595B078","customerContact"),  //维保公司名
+        Contact_Email("4AFC9CD0CD8C4AA7B9921B6EC4645B95","contactEmial"),   //邮件
+        CC_Email("2AEAA42EFDC34CC18C2E8BA2EC498550","ccEmail"),   //邮件
+        Contact_Number("EB7AF9DC6E8D49FC98DC67ACE96BFFB6","contactNumber"), //number
+        Customer_Address("9AD25F4EAEE54EAC86D6D7DD2E2FDA2E","customerAddress"), //安装地址
+        Business_Name("A20288A9EAD24CC28564B4EEA7B7968F","businessName"), //终端客户
+        Reference("0BB75D000E45404FAA3E2AB8189B1301","reference"),
+        Electrical_Compliance_Certificate("1029856393994F4695065FB1A76D00B9","electricalComplianceCertificate"),
+        Invoice_Uploaded("88D2D00FB3544918942470A7A3F6281C","invliceUpload"),
+        Fault_Type("CF85B3DC233E4B70852F409812713A37","faultType"),
+        Fault_Description("D31C9BE662904521A5A7577311D282FF","faultDescription"),
+        Status_Message_LCD("99763AADA25D422089AAAB338C49E848","lcdMessage"),
+        Inverter_Exposed_Weather("776769F766484874ADD9354E01AF0F16","isWeather"),
+        Intervter_Connect_Battery("B029CDE660DA4053BACC2322E90B29CD","battery"),
+        Post_Code("49D5913D6552439FABBCE76A26B57014","postCode"),
+        Country("48B4E9768FE14BC9B60D231B80893A36","contryCode"),
+        AMOUNT_EXCL_GST("DF5B2DC1E98F4509B11D866FFB2371C2","amountGST"),
+        GST("0A652A2381B94F5A8BA42C97970201EB","gst"),
+        TOTAL_AMOUNT("0CF10A5B6EA94732A8253B0C801E9249","totalAmount"),
+        PURCHASE_ORDER("248246FD02874D34B0CA85E8996C0DA4","purchaseOrder"),
+        SHIPPING_ADDRESS("042D3DDDD048464EA6B39E31527B926F","shippingAddress"),
+        CURRENCY_CODE("4A3E10C5776048BC984D45DA47026A37","currency"),
+        INVERTER_INSTALLATION_LOCATION("E6DB2E39349C4FEBB681500A674DF80D","location"),
+        BATTERY_MODEL("C65359DD9C8B49BFA12462263498CFF7","batteryModel"),
+
+        Product_id("8E6F00CE608B410AADCFBC378B83F4C2",""),
+        Delivery_date("5D66B7A7012843A49E9B2FF11F49C972",""),
+        Dispatched_Date("8073A0B54A9F492BABC49D02EDB48BA6",""),
+        accessory("528203E7CF6B4695BF9CD6CA6B9D0E14","accessory"),
+
+        Parts_Warranty_W5YP("A1B8254717004CF8A40E405B216352B5","W5YP"),
+        Parts_Warranty_W5YS("31FF6AE5353D4D498A7C68EF20454456","W5YS"),
+        ;
+
+        private String id;
+        private String key;
+
+        public static String getId(String key){
+            for(API_PARAMS e : API_PARAMS.values()){
+                if(key.equals(e.getKey())){
+                    return e.getId();
+                }
+            }
+            return null;
+        }
+
+        API_PARAMS(String id, String key){
+            this.id = id;
+            this.key = key;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getKey() {
+            return key;
+        }
+    }
 
     public enum API_HEADER_DTOS {
         EQUIPMENT20("Equipment.20"),
